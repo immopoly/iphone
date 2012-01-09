5a580b7f389f0453818f19fde55a537914d8bd22
Tobias Buchholz <tobias.buchholz89@googlemail.com>
2012-01-09 16:29:40 +0100

    Refactor: helperbutton fix

--- ImmopolyIPhone/ImmopolyIPhone/AbstractViewController.m
+++ ImmopolyIPhone/ImmopolyIPhone/AbstractViewController.m
@@ -161,6 +161,7 @@
         [UIView commitAnimations];
         
         [self setViewIsVisible:YES];
+        [btHelperViewIn setEnabled:NO];
     }
 }
 
@@ -173,6 +174,7 @@
     [UIView commitAnimations];
     
     [self setViewIsVisible:NO];
+    [btHelperViewIn setEnabled:YES];
 }
 
 /*- (void)setHelperViewTitle:(NSString *)_viewTitle {
