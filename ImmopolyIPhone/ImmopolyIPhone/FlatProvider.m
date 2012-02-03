e023c189dea91eaf7d3411c874dadef0220c3a8f
Tobias Heine <spam@tobiasheine.eu>
2012-02-03 19:31:14 +0100

    refactor FlatProvider

--- ImmopolyIPhone/ImmopolyIPhone/FlatProvider.m
+++ ImmopolyIPhone/ImmopolyIPhone/FlatProvider.m
@@ -14,15 +14,9 @@
 
 #define NUMBER_OF_PAGES 4
 
-@interface FlatProvider() {
-    // counter variable for getting flats from location of 5 pages
-    int pageNum; 
-}
-
-@end
-
 @implementation FlatProvider
 
+@synthesize location,pageNum;
 
 - (void)getExposeFromId:(int)_exposeId {
     OAuthManager *manager = [[OAuthManager alloc] init];
@@ -36,36 +30,50 @@
 
 
 - (void)getFlatsFromLocation:(CLLocationCoordinate2D)_location {
+    pageNum=1;
+    [self setLocation:_location];
+    [self getFlatsFromLocationAndPageNumber:pageNum];
+    
+}
+
+- (void)getFlatsFromLocationAndPageNumber:(int)_pageNumber{
     OAuthManager *manager = [[OAuthManager alloc] init];
     NSString *url; 
     
-    for(pageNum=1; pageNum<=NUMBER_OF_PAGES; pageNum++) {
-        url = [[NSString alloc]initWithFormat:@"%@search/radius.json?realEstateType=apartmentrent&pagenumber=%d&geocoordinates=%f;%f;3.0",urlIS24API, pageNum, _location.latitude, _location.longitude];
+    url = [[NSString alloc]initWithFormat:@"%@search/radius.json?realEstateType=apartmentrent&pagenumber=%d&geocoordinates=%f;%f;3.0",urlIS24API, pageNum, self.location.latitude, self.location.longitude];
     
-        [manager grabURLInBackground:url withFormat:@"application/json" withDelegate:self];
-    }
+    [manager grabURLInBackground:url withFormat:@"application/json" withDelegate:self];
+    
     
     [manager release];
     [url release];
-    pageNum=1;
+    
+    
 }
 
 - (void)requestFinished:(ASIHTTPRequest *)request {
     NSString *responseString = [request responseString];
     NSError *err=nil;
+    
+    NSArray *flats = [JSONParser parseFlatData:responseString :&err];
+    
     [[[ImmopolyManager instance] immoScoutFlats] addObjectsFromArray:[JSONParser parseFlatData:responseString :&err]];
     
     if (err) {
         //Handle Error here
         NSDictionary *errorInfo = [NSDictionary dictionaryWithObject:err forKey:@"error"];
         [[NSNotificationCenter defaultCenter] postNotificationName:@"flatProvider/parse fail" object:nil userInfo:errorInfo];
-    }else{
-        if(pageNum == NUMBER_OF_PAGES) {
-            [[ImmopolyManager instance] callFlatsDelegate];
-            NSLog(@"Response: %@",responseString);    
-        }
     }
-    pageNum++;
+    
+    if(pageNum == NUMBER_OF_PAGES || [flats count] == 0 ){
+        [[ImmopolyManager instance] callFlatsDelegate];
+        NSLog(@"Response: %@",responseString);    
+    }else{
+        pageNum++;
+        [self getFlatsFromLocationAndPageNumber:pageNum];
+    }
+    
+    
 }
 
 - (void)requestFailed:(ASIHTTPRequest *)request {
