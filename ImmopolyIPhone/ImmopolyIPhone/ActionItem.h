74130e603f983e17640fa92efdc07c6315b67490
Tobias Heine <spam@tobiasheine.eu>
2012-03-25 16:52:40 +0200

    ActionItem freie Wohnungen anzeigen
    object template

---
+++ ImmopolyIPhone/ImmopolyIPhone/ActionItem.h
@@ -0,0 +1,13 @@
+//
+//  ActionItem.h
+//  ImmopolyIPhone
+//
+//  Created by Tobias Heine on 25.03.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ActionItem : NSObject
+
+@end
---
+++ ImmopolyIPhone/ImmopolyIPhone/ActionItem.m
@@ -0,0 +1,13 @@
+//
+//  ActionItem.m
+//  ImmopolyIPhone
+//
+//  Created by Tobias Heine on 25.03.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ActionItem.h"
+
+@implementation ActionItem
+
+@end
