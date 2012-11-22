74d1499d5abcd85ba0cce4e362c3b587a5301151
TobiasBuchholz <tobias.buchholz89@googlemail.com>
2012-11-22 21:47:59 +0100

    changed callout bubble layout

--- ImmopolyIPhone/ImmopolyIPhone/PortfolioViewController.m
+++ ImmopolyIPhone/ImmopolyIPhone/PortfolioViewController.m
@@ -636,36 +636,39 @@ static NSString *ANNO_IMG_MULTI = @"Haus_cluster_hpdi.png";
     UIView *subview = [[UIView alloc] initWithFrame:frame];
     subview.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:0];
     
+    UIColor *textColor = [UIColor colorWithRed:63.0/255.0 green:100.0/255.0 blue:148.0/255.0 alpha:1];
+    UIColor *backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:0];
+    
     //labels
-    UILabel *lbName = [[UILabel alloc] initWithFrame:CGRectMake(10, -20, 193, 70)];
+    UILabel *lbName = [[UILabel alloc] initWithFrame:CGRectMake(10, -15, 193, 70)];
     [lbName setFont:[UIFont fontWithName:@"Arial Rounded MT Bold" size:(12.0)]];
-    [lbName setBackgroundColor:[UIColor colorWithRed:0 green:0 blue:0 alpha:0]];
+    [lbName setBackgroundColor:backgroundColor];
     [lbName setNumberOfLines:2];
-    [lbName setTextColor:[UIColor colorWithRed:1 green:1 blue:1 alpha:1]];
+    [lbName setTextColor:[UIColor colorWithRed:40.0/255.0 green:77.0/255.0 blue:125.0/255.0 alpha:1]];
     [lbName setText:[_flat name]];
     [subview addSubview:lbName];
     
-    UILabel *lbRooms = [[UILabel alloc] initWithFrame:CGRectMake(90, 30, 100, 35)];
+    UILabel *lbRooms = [[UILabel alloc] initWithFrame:CGRectMake(90, 35, 100, 35)];
     [lbRooms setFont:[UIFont fontWithName:@"Arial Rounded MT Bold" size:(12.0)]];
     NSString *rooms = [NSString stringWithFormat:@"Zimmer: %d",[_flat numberOfRooms]];
-    [lbRooms setBackgroundColor:[UIColor colorWithRed:0 green:0 blue:0 alpha:0]];
-    [lbRooms setTextColor:[UIColor colorWithRed:1 green:1 blue:1 alpha:1]];
+    [lbRooms setBackgroundColor:backgroundColor];
+    [lbRooms setTextColor:textColor];
     [lbRooms setText:rooms];
     [subview addSubview:lbRooms];
     
-    UILabel *lbSpace = [[UILabel alloc] initWithFrame:CGRectMake(90, 55, 200, 35)];
+    UILabel *lbSpace = [[UILabel alloc] initWithFrame:CGRectMake(90, 60, 200, 35)];
     NSString *space = [NSString stringWithFormat:@"Fläche: %.2f m²",[_flat livingSpace]];
     [lbSpace setFont:[UIFont fontWithName:@"Arial Rounded MT Bold" size:(12.0)]];
-    [lbSpace setBackgroundColor:[UIColor colorWithRed:0 green:0 blue:0 alpha:0]];
-    [lbSpace setTextColor:[UIColor colorWithRed:1 green:1 blue:1 alpha:1]];
+    [lbSpace setBackgroundColor:backgroundColor];
+    [lbSpace setTextColor:textColor];
     [lbSpace setText:space];
     [subview addSubview:lbSpace];
     
-    UILabel *lbPrice = [[UILabel alloc] initWithFrame:CGRectMake(90, 80, 200, 35)];
+    UILabel *lbPrice = [[UILabel alloc] initWithFrame:CGRectMake(90, 85, 200, 35)];
     NSString *price = [NSString stringWithFormat:@"Preis: %.2f €",[_flat price]];
     [lbPrice setFont:[UIFont fontWithName:@"Arial Rounded MT Bold" size:(12.0)]];
-    [lbPrice setBackgroundColor:[UIColor colorWithRed:0 green:0 blue:0 alpha:0]];
-    [lbPrice setTextColor:[UIColor colorWithRed:1 green:1 blue:1 alpha:1]];
+    [lbPrice setBackgroundColor:backgroundColor];
+    [lbPrice setTextColor:textColor];
     [lbPrice setText:price];
     [subview addSubview:lbPrice];
     
